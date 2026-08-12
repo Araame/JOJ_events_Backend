5e63f8fa2534f87411dd9c8c658dd3c6abe7a128
Birane LY <biranely94@gmail.com>
2026-08-12 09:47:07 +0000

    feat: Création des modèles pour chaque application ainsi que la configuration du settings.

--- paiements/models.py
+++ paiements/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Spectateur(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    email = models.EmailField()
+    tel = models.CharField(max_length=20)
+
+class Transaction(models.Model):
+    numero_transaction = models.CharField(max_length=100, unique=True)
+    mode_paiement = models.CharField(max_length=50)
+    montant = models.DecimalField(max_digits=10, decimal_places=2)
+    telephone = models.CharField(max_length=20)
+    date = models.DateTimeField(auto_now_add=True)
+
+class Billet(models.Model):
+    spectateur = models.ForeignKey(Spectateur, on_delete=models.CASCADE, related_name='billets')
+    zone = models.ForeignKey('sites.Zone', on_delete=models.CASCADE, related_name='billets')
+    evenement = models.ForeignKey('evenements.Evenement', on_delete=models.CASCADE)
+    transaction = models.OneToOneField(Transaction, on_delete=models.CASCADE, related_name='billet')
+    qr_code = models.CharField(max_length=255, unique=True)
+    place = models.CharField(max_length=50)
--- sites/models.py
+++ sites/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Site(models.Model):
+  nom= models.CharField(max_length=255)
+  capacite = models.PositiveIntegerField()
+  description = models.TextField(blank=True)
+  service = models.TextField(blank=True)
+  image = models.ImageField(upload_to='sites/', blank=True, null=True)
+  latitude = models.FloatField(blank=True, null=True)
+  longitude = models.FloatField(blank=True, null=True)
+  ville = models.CharField(max_length=100)
+  region = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.nom
+
+
+class Zone(models.Model):
+  site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='zones')
+  nom = models.CharField(max_length=100)
+  capacite = models.PositiveIntegerField()
+
+  def __str__(self):
+    return f"{self.nom} - {self.site.nom}"
+
+
