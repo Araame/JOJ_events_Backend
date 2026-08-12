8cd2452e7545cae0e4c0b1fe777f139e1675a5f2
Adji Aissatou Wade SAMBE <adjiaissatouwade@gmail.com>
2026-08-12 17:56:44 +0000

    Ajout des urls dans l'app evenements

---
+++ evenements/urls.py
@@ -0,0 +1,11 @@
+# evenements/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ResultatViewSet
+
+router = DefaultRouter()
+router.register(r'resultats', ResultatViewSet, basename='resultat')
+
+urlpatterns = [
+    path('api/resultats/', include(router.urls)),
+]
