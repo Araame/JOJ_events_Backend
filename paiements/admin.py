f08cf739349c5e7888e316ba2433a534edb1f761
Birane LY <biranely94@gmail.com>
2026-08-12 16:31:53 +0000

    feat: Configuration de l'interface admin de Django

--- paiements/admin.py
+++ paiements/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Spectateur, Transaction, Billet
 
-# Register your models here.
+
+@admin.register(Spectateur)
+class SpectateurAdmin(admin.ModelAdmin):
+    list_display = ('prenom', 'nom', 'email', 'tel')
+    search_fields = ('nom', 'prenom', 'email', 'tel')
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ( 'mode_paiement', 'montant', 'telephone', 'date')
+    list_filter = ('mode_paiement', 'date')
+    search_fields = ('telephone',)
+    readonly_fields = ('date',)
+
+
+@admin.register(Billet)
+class BilletAdmin(admin.ModelAdmin):
+    list_display = ('qr_code', 'spectateur', 'evenement', 'zone', 'place')
+    search_fields = ('qr_code', 'spectateur__nom', 'spectateur__email')
+    list_filter = ('evenement', 'zone')
+    readonly_fields = ('qr_code',)
