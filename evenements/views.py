b80c798398b808fc828ddd3522f67560d7b908f1
Araame <aram171002@gmail.com>
2026-08-14 12:11:10 +0000

    feat: evenements viewset retrived

--- evenements/views.py
+++ evenements/views.py
@@ -258,3 +258,37 @@ class JoueurViewSet(viewsets.ModelViewSet):
             permission_classes = [permissions.AllowAny]
         return [permission() for permission in permission_classes]
 
+
+
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Evenement
+from .serializers import EvenementSerializer,EvenementListSerializer
+
+from .eventFiltre import EventFiltre
+from django_filters.rest_framework import DjangoFilterBackend
+from .pagination import EvenementPagination
+from .permissions import IsAdminOrReadOnly
+
+class Evenements(viewsets.ModelViewSet):
+    queryset = Evenement.objects.all()
+    def get_serializer_class(self):
+        #get
+        if self.action =='list':
+            return EvenementListSerializer
+        # POST, GET détail, PUT, PATCH
+        return EvenementSerializer
+    
+    
+    
+    permission_classes = [IsAdminOrReadOnly]
+    #systeme de filtrage
+    filter_backends = [DjangoFilterBackend]
+    # regle de filtrage
+    filterset_class = EventFiltre
+    #pagination
+    pagination_class = EvenementPagination
+        
+        
+    
+    
