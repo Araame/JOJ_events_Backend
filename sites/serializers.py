021534d4fd8cafded041fce762382764005085df
Birane LY <biranely94@gmail.com>
2026-08-18 17:54:54 +0000

    fix: Correction de quelques conflits de merge dans la view et l'urms.py de l'application evenements ainsi que dans le fichier serializer de l'application sites.

--- sites/serializers.py
+++ sites/serializers.py
@@ -1,49 +1,6 @@
 from rest_framework import serializers
 from .models import Site, Zone
 
-<<<<<<< HEAD
-# Create your views here.
-class ZoneSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Zone
-        fields = ['id', 'nom']  
-
-class SiteSerializer(serializers.ModelSerializer):
-    zones = ZoneSerializer(many=True, read_only=True) 
-
-    class Meta:
-        model = Site
-        fields = '__all__'  
-
-    def create(self, validated_data):
-        zones_data = validated_data.pop('zones', [])
-        
-        # Créer le site
-        site = Site.objects.create(**validated_data)
-        
-        # Créer les zones associées
-        for zone_data in zones_data:
-            Zone.objects.create(site=site, **zone_data)
-            
-        return site
-
-    def update(self, instance, validated_data):
-        # Récupérer les nouvelles données des zones
-        zones_data = validated_data.pop('zones', None)
-        
-        # Mise à jour simple du site
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-        
-        # Mise à jour des zones 
-        if zones_data is not None:
-            instance.zone_set.all().delete()
-            for zone_data in zones_data:
-                Zone.objects.create(site=instance, **zone_data)
-                
-        return instance
-=======
 
 class SiteSerializer(serializers.ModelSerializer):
     """Serializer de gestion des sites sportifs."""
@@ -140,4 +97,3 @@ class ZoneSerializer(serializers.ModelSerializer):
             })
 
         return data
->>>>>>> origin/features/api-jd
